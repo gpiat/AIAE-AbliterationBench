8b36cd5b7cf38757f24eaf96167b58fffbf8dac1
gpiat <guilhem.piat@protonmail.com>
2025-02-01 22:32:31 +0100

    undid rename (it caused issues)

--- ablit_bench.py
+++ ablit_bench.py
@@ -8,8 +8,8 @@ import torch
 
 from barplot import plot_refusal_scores
 from datasets import load_dataset
-from erismod.eris_forge import Forge
-from erismod.scorers.refusal_scorer.expression_refusal_scorer import ExpressionRefusalScorer
+from erisforge.eris_forge import Forge
+from erisforge.scorers.refusal_scorer.expression_refusal_scorer import ExpressionRefusalScorer
 from sklearn.model_selection import train_test_split
 from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
