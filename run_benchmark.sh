c9662badb162c4a8b8182be33bdb1a79122d71ac
gpiat <guilhem.piat@protonmail.com>
2025-02-01 21:40:56 +0100

    fix typo

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -7,7 +7,7 @@
 #   - Chat templates should be supported by the models' tokenizers. The code shouldn't crash, but you may get unexpected results.
 #   - [Qwen 1.5 0.5B Chat] and [gemma 1.1 2b it] have been verified as working correctly.
 
-python ablit_bench \
+python ablit_bench.py \
   -n Qwen/Qwen-1.5-0.5B-Chat google/gemma-1.1-2b-it \
   -l 2 \
   -i 20 \
