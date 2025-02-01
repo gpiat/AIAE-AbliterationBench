6c9c5f07ce8ffd2d20304b74d465763d5f2bed07
DamiFass <dami.fassina@gmail.com>
2025-02-01 13:46:37 +0000

    Fixed hardcoded path

--- try.py
+++ try.py
@@ -1,5 +1,7 @@
+import os
 import sys
-sys.path.append('/Users/damifass/Desktop')
+# sys.path.append('/Users/damifass/Desktop')
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import platform
 import torch
